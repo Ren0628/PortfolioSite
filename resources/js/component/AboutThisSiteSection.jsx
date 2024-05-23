30c2ffb0df902e03213f59eb1da59c2f0b02fee3
Ren0628 <ren6nt@gmail.com>
2024-05-23 23:38:44 +0900

    About this site セクション作成

---
+++ resources/js/component/AboutThisSiteSection.jsx
@@ -0,0 +1,16 @@
+function AboutThisSiteSection() {
+    return (
+        <div id="AboutThisSite" className="about_this_site py-64 px-12 md:px-24 container mx-auto">
+            <h2 className="text-4xl md:text-5xl text-center font-bold mb-10">
+                About this site
+            </h2>
+            <p className="mx-auto leading-8">
+                このポートフォリオサイトは、エンジニアになるために約1年間にわたり学んできた内容をまとめたものです。
+                学習の成果や制作物、私自身について詳しく紹介しています。
+                ぜひご覧いただき、私の成長の過程とスキルを見ていただければと思います。
+            </p>
+        </div>
+    );
+}
+
+export default AboutThisSiteSection;
