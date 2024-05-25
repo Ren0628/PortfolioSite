76866fd2508b321799ed2f97616afac8eaa7f480
Ren0628 <ren6nt@gmail.com>
2024-05-25 16:01:16 +0900

    リンク変更

--- resources/js/component/Header.jsx
+++ resources/js/component/Header.jsx
@@ -2,20 +2,20 @@ import BurgerMenu from "./BurgerMenu";
 
 export default function Header() {
     return (
-        <header className="fixed top-0 left-0 right-0 bg-white bg-opacity-60">
+        <header className="fixed top-0 left-0 right-0 bg-white bg-opacity-80">
             <nav className="flex justify-between mx-auto container">
-                <div className="text-3xl ml-8 font-bold"><a className="flex h-full items-center" href="#">Portfolio</a></div>
+                <div className="text-3xl ml-8 font-bold"><a className="site_logo flex h-full items-center" href="#">Dev.</a></div>
                 <ul className="space-x-10 font-bold mr-8 text-lg hidden md:flex">
                     <li><a className="flex h-full items-center py-4" href="#">Top</a></li>
                     <li><a className="flex h-full items-center py-4" href="#AboutThisSite">About this site</a></li>
                     <li className="relative group">
-                        <a className="flex h-full items-center py-4" href="#">About me</a>
-                        <ul className="w-28 absolute inset-x-1/2 bg-white bg-opacity-60 rounded-b-md invisible group-hover:visible opacity-0 group-hover:opacity-100 -translate-y-2 -translate-x-1/2 group-hover:translate-y-0 ease-in-out duration-500">
-                            <li><a className="flex h-full items-center justify-center py-2 mx-8" href="#">Works</a></li>
-                            <li><a className="flex h-full items-center justify-center py-2 mx-9" href="#">Slills</a></li>
+                        <a className="flex h-full items-center py-4" href="#AboutMe">About me</a>
+                        <ul className="w-28 absolute inset-x-1/2 bg-white bg-opacity-80 rounded-b-md invisible group-hover:visible opacity-0 group-hover:opacity-100 -translate-y-2 -translate-x-1/2 group-hover:translate-y-0 ease-in-out duration-500">
+                            <li><a className="flex h-full items-center justify-center py-2 mx-8" href="#Works">Works</a></li>
+                            <li><a className="flex h-full items-center justify-center py-2 mx-9" href="#Skills">Slills</a></li>
                         </ul>
                     </li>
-                    <li><a className="flex h-full items-center py-4" href="#">Contact</a></li>
+                    <li><a className="flex h-full items-center py-4" href="#Contact">Contact</a></li>
                 </ul>
                 <BurgerMenu />
             </nav>
--- resources/js/component/NavSidebar.jsx
+++ resources/js/component/NavSidebar.jsx
@@ -1,15 +1,16 @@
 
-function NavSidebar({ NavSidebarOpen }) {
+function NavSidebar({ handleClose, NavSidebarOpen }) {
 
   return (
     <div className={`NavSidebar pt-8 pl-20 ${NavSidebarOpen && 'active'}`}>
+      <div className="flex justify-end pr-20 mb-4"><i onClick={handleClose} className="fa-solid fa-xmark fa-2x cursor-pointer"></i></div>
         <ul className="font-bold text-lg">
             <li><a className="inline-block mb-3" href="#">Top</a></li>
             <li><a className="inline-block mb-3" href="#AboutThisSite">About this site</a></li>
-            <li><a className="inline-block mb-3" href="#">About me</a></li>
-            <li><a className="inline-block mb-3" href="#">Works</a></li>
-            <li><a className="inline-block mb-3" href="#">Slills</a></li>
-            <li><a className="inline-block mb-3" href="#">Contact</a></li>
+            <li><a className="inline-block mb-3" href="#AboutMe">About me</a></li>
+            <li><a className="inline-block mb-3" href="#Works">Works</a></li>
+            <li><a className="inline-block mb-3" href="#Skills">Slills</a></li>
+            <li><a className="inline-block mb-3" href="#Contact">Contact</a></li>
         </ul>
     </div>
   )
