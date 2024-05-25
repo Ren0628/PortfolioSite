55a12e22ee9b95e96940ecb8c6c870ecece18782
Ren0628 <ren6nt@gmail.com>
2024-05-25 15:58:53 +0900

    footer作成

---
+++ resources/js/component/Footer.jsx
@@ -0,0 +1,10 @@
+
+function Footer() {
+  return (
+    <footer className="h-16 pl-4 md:pl-20 flex items-center">
+        <p className="text-sm text-white">&copy; 2024 Tawa Ren. All rights reserved.</p>
+    </footer>
+  )
+}
+
+export default Footer
