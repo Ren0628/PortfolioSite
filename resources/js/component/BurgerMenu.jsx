01ea010d7ac3bf4205a44a7eaacf63caf54b8c0f
Ren0628 <ren6nt@gmail.com>
2024-05-25 16:00:49 +0900

    コード改善

--- resources/js/component/BurgerMenu.jsx
+++ resources/js/component/BurgerMenu.jsx
@@ -4,25 +4,22 @@ import GrayWrap from "@/component/GrayWrap";
 
 function BurgerMenu() {
     const [NavSidebarOpen, setNavSidebarOpen] = useState(false);
-    const [WrapOn, setWrapOn] = useState(false);
 
     const handleNavSidebarOpen = () => {
-        setWrapOn(true);
         setNavSidebarOpen(true);
     }
 
     const handleNavSidebarClose = () => {
-        setWrapOn(false);
         setNavSidebarOpen(false);
     }
 
     return (
         <>
             <div onClick={handleNavSidebarOpen} className="md:hidden mr-8 py-4">
-                <i className="fa-solid fa-bars-staggered fa-2x cursor-pointer"></i>
+                <i className="fa-solid fa-bars fa-2x cursor-pointer"></i>
             </div>
-            <NavSidebar NavSidebarOpen={NavSidebarOpen} />
-            {WrapOn && <GrayWrap handleClose={handleNavSidebarClose} />}
+            <NavSidebar handleClose={handleNavSidebarClose} NavSidebarOpen={NavSidebarOpen} />
+            {NavSidebarOpen && <GrayWrap handleClose={handleNavSidebarClose} />}
         </>
     );
 }
