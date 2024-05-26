3efe52d0cbb1d851cca6f33785c1bbc1cfa7dc6e
Ren0628 <ren6nt@gmail.com>
2024-05-26 16:36:41 +0900

    Works追加

--- resources/js/component/WorksSection.jsx
+++ resources/js/component/WorksSection.jsx
@@ -20,6 +20,15 @@ function WorksSection({ imageUrl }) {
                         appLink={""}
                         appComment={"このアプリは、手形帳の入力を簡単にしたいという依頼を受けて作成したものです。LaravelとMySQLを使用してCRUD機能を実装し、JavaScriptのfetch APIを利用して入力フォームで振出人を入力すると関連情報が非同期で自動補完されるようにしました。さらに、前月の入力データをdomPDFでPDF化する処理をコマンドとして実装し、そのコマンドを呼び出すbatファイルを作成しました。これにより、Windowsのタスクスケジューラーを使って毎月自動的にPDFが保存されるようにしました。"}
                     />
+                    <WorksItem
+                        imageUrl={imageUrl}
+                        imageName={"restaurant"}
+                        appName={"Sample App"}
+                        technicalStack={"HTML/CSS, BootStrap, JavaScript, PHP, Laravel, MySQL, AWS"}
+                        gitHubUrl={"https://github.com/Ren0628/restaurant-app"}
+                        appLink={""}
+                        appComment={"このアプリは、レストランの投稿、レビュー、お気に入り登録ができるサンプルアプリです。LaravelのGuard機能を使用して、アカウントをユーザー、オーナー、アドミンに分類し、それぞれの役割に応じたページと操作を実装しました。オーナーアカウントでは、レストランの登録と削除が可能です。ユーザーアカウントでは、各レストランのお気に入り登録とレビューの投稿ができ、マイページでお気に入りのレストランを確認できます。また、アドミンアカウントでは、レストランの削除やレビューの削除、レストランジャンルの追加などの管理作業が行えます。"}
+                    />
                 </div>
             </div>
         </div>
